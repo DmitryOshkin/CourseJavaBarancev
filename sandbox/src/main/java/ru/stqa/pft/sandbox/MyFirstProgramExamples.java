c77761ea9275c78e4b88d40e1e8fbb42cf6bd012
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2020-07-21 15:59:07 +0300

    исправление орфографических ошибок

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramExamples.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramExamples.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramExamples {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello, world!");
 
     System.out.println(2 + 2);
     System.out.println(2 * 2);
